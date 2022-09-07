fa60b04621f5bc91c4af7becd22055f32d2b41c4
Jadson Santos <jadsondossantos5@gmail.com>
2022-09-06 23:58:22 -0300

    update header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,14 @@ import React from 'react';
 import "./index.scss"
 
 const Header = () => {
-  return <header className="header">App React</header>;
+  return (
+    <header className="header">
+      <nav>
+        <a href="/">Home</a>
+        <a href="/login">Login / Criar</a>
+      </nav>
+    </header>
+  )
 };
 
 export default Header;
