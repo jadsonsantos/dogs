913d625c752fd2ad892df321d2a7f971fc674258
Jadson Santos <jadsondossantos5@gmail.com>
2022-09-08 09:42:20 -0300

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./App.scss"
+import Footer from './components/Footer';
 import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div>
       <Header />
-      App React
+      <Footer />
     </div>
   )
 };
