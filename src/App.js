f501d963302df63572e6bfeec9969656d127c3f5
Jadson Santos <jadsondossantos5@gmail.com>
2022-11-02 16:47:34 -0300

    add browser router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 import Footer from "./components/Footer"
 import Header from "./components/Header/Header"
 import "./App.scss"
 
 const App = () => (
   <div>
-    <Header />
-    <Footer />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </div>
 )
 
