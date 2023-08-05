f4d726f4c090c98f543fb954c2937b4156f05342
Jadson Santos <jadsondossantos5@gmail.com>
2023-08-05 16:23:40 -0300

    header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,17 @@
 import React from "react"
-// import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { ReactComponent as Dogs } from "../../assets/dogs.svg"
 import "./Header.scss"
 
 const Header = () => (
   <header className="header">
-    <nav className="menu container">
-      {/* <Link to="/">Home</Link> */}
-      <a href="/">Home</a>
-      <a href="/login">Login / Criar</a>
+    <nav className="header__nav container">
+      <Link to="/" aria-label="Dogs - Home" className="header__logo">
+        <Dogs />
+      </Link>
+      <Link to="/login" className="header__login">
+        Login / Criar
+      </Link>
     </nav>
   </header>
 )
