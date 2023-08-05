801714f4ffe6701a4eef103a5b631701772f09c4
Jadson Santos <jadsondossantos5@gmail.com>
2023-08-05 16:22:39 -0300

    login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,3 +1,12 @@
-const Login = () => <div className="container">Login</div>
+import { Route, Routes } from "react-router-dom"
+import LoginForm from "../../components/LoginForm"
+
+const Login = () => (
+  <div className="container">
+    <Routes>
+      <Route path="/" element={<LoginForm />} />
+    </Routes>
+  </div>
+)
 
 export default Login
