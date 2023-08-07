36264f98d914943634636fe3b77bb588516d88ae
Jadson Santos <jadsondossantos5@gmail.com>
2023-08-06 21:42:50 -0300

    login background

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import LoginForm from "../../components/LoginForm"
+import "./Login.scss"
 
 const Login = () => (
-  <div className="container">
-    <Routes>
-      <Route path="/" element={<LoginForm />} />
-    </Routes>
+  <div className="login">
+    <div className="login__forms container">
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+      </Routes>
+    </div>
   </div>
 )
 
