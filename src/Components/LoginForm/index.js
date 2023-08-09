91de41d36de485e1822d9cfefbbef8782df7c236
Jadson Santos <jadsondossantos5@gmail.com>
2023-08-09 16:59:07 -0300

    title and animeLeft classes

--- src/Components/LoginForm/index.js
+++ src/Components/LoginForm/index.js
@@ -43,9 +43,9 @@ const LoginForm = () => {
   }, [])
 
   return (
-    <section>
+    <section className="animeLeft">
       <Head title="Login" />
-      <h1>Login</h1>
+      <h1 className="title">Login</h1>
       <form onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
