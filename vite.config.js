fd060e297e3c4cc95fa9355d085a721ae83a0994
Jadson Santos <jadsondossantos5@gmail.com>
2026-03-19 16:51:58 -0300

    ajusta imports

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
+import { fileURLToPath } from "url"
+import { defineConfig } from "vite"
 import svgr from "vite-plugin-svgr"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      pages: path.resolve(__dirname, "src/pages"),
+      components: path.resolve(__dirname, "src/components"),
+      contexts: path.resolve(__dirname, "src/contexts"),
+      hooks: path.resolve(__dirname, "src/hooks"),
+      assets: path.resolve(__dirname, "src/assets"),
+    },
+  },
 })
